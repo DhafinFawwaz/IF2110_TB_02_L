be0b78bf084f948a3dfb29ec4d9b7c062da72ef4
Intermaze <rafikiprawhira@gmail.com>
2023-10-30 09:56:40 +0700

    feat: main display

--- main.c
+++ main.c
@@ -1,8 +1,20 @@
 #include "Fitur/Inisialisasi/inisialisasi.h"
+#include <stdio.h>
 
 
 int main()
 {
+    printf(".______    __    __  .______      .______    __  .______ \n");
+    printf("|   _  \\  |  |  |  | |   _  \\     |   _  \\  |  | |   _  \\ \n");
+    printf("|  |_)  | |  |  |  | |  |_)  |    |  |_)  | |  | |  |_)  | \n");
+    printf("|   _  <  |  |  |  | |      /     |   _  <  |  | |      / \n");
+    printf("|  |_)  | |  `--'  | |  |\\  \\----.|  |_)  | |  | |  |\\  \\----. \n");
+    printf("|______/   \\______/  | _| `._____||______/  |__| | _| `._____| \n\n");
+
+    printf("Selamat datang di BurBir. \n\n");
+    printf("Aplikasi untuk studi kualitatif mengenai perilaku manusia dengan menggunakan metode \n");
+    printf("(pengambilan data berupa) Focused Group Discussion kedua di zamannya. \n\n");
+
     inisialisasi();
     return 0;
 }
