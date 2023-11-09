52fbd3c38759024d707e76d25611301b98587550
novelxv <novelyaputriramadhani11@gmail.com>
2023-11-09 10:11:21 +0700

    Add: ADT Profile

--- ADT/Profile/profile.c
+++ ADT/Profile/profile.c
@@ -1,8 +1,112 @@
+/* File: profile.c */
+/* ADT Profile yang terdiri dari bio akun, nomor HP, weton, jenis akun, dan foto profil dari pengguna */
+
+#include <stdio.h>
 #include "profile.h"
 
-void CreateProfile(Profile* profile){
-    CreateFoto(&(profile->foto));
+/* *** PROTOTYPE *** */
+/* *** PEMBUATAN DENGAN NILAI DEFAULT *** */
+void CreateProfil(Profile *P){
+    /* Mambuat profil kosong dengan nilai-nilai default */
+    setWord(&Bio(*P), "");
+    setWord(&NomorHP(*P), "");
+    setWord(&Weton(*P), "");
+    setWord(&JenisAkun(*P), "Publik");
+    CreateFotoProfil(&Foto(*P));
 }
-void DisplayProfile(Profile profile){
-    
+
+/* *** PEMERIKSAAN NILAI *** */
+boolean isWetonValid(Word weton){
+    /* Memeriksa apakah weton valid */
+    // Konversi semua huruf ke huruf kecil
+    Word weton_kecil = weton;
+    for (int i = 0; i < weton.Length; i++){
+        if (weton_kecil.TabWord[i] >= 'A' && weton_kecil.TabWord[i] <= 'Z'){
+            weton_kecil.TabWord[i] += ('a' - 'A');
+        }
+    }
+    return (compareWord(weton_kecil, stringToWord("pahing")) ||
+            compareWord(weton_kecil, stringToWord("kliwon")) ||
+            compareWord(weton_kecil, stringToWord("wage")) ||
+            compareWord(weton_kecil, stringToWord("pon")) ||
+            compareWord(weton_kecil, stringToWord("legi")));
+}
+
+boolean isNomorHPValid(Word nomor_hp){
+    /* Memeriksa apakah nomor HP valid */
+    for (int i = 0; i < nomor_hp.Length; i++){
+        if (!(nomor_hp.TabWord[i] >= '0' && nomor_hp.TabWord[i] <= '9')){
+            return false;
+        }
+    }
+    return true;
+}
+
+boolean isAkunPrivat(Profile P){
+    /* Memeriksa apakah jenis akun privat */
+    return (compareWord(JenisAkun(P), stringToWord("Privat")));
+}
+
+boolean isAkunPublik(Profile P){
+    /* Memeriksa apakah jenis akun publik */
+    return (compareWord(JenisAkun(P), stringToWord("Publik")));
+}
+
+/* *** PENGUBAHAN NILAI *** */
+void changeBio(Profile *P, Word w){
+    /* Mengubah bio berdasarkan masukan pengguna */
+    if (w.Length > 135){
+        printf("\nPanjang bio melebihi batas maksimum. Karakter ke 136 dan seterusnya akan dihapus.\n");
+    }
+    capWord(w, 135);
+    Bio(*P) = w;
+}
+
+void changeNomorHP(Profile *P, Word w){
+    /* Mengubah nomor HP berdasarkan masukan pengguna */
+    NomorHP(*P) = w;
+}
+
+void changeWeton(Profile *P, Word w){
+    /* Mengubah weton berdasarkan masukan pengguna */
+    Weton(*P) = w;
+}
+
+void changeJenisAkun(Profile *P){
+    /* Mengubah jenis akun pengguna */
+    if (compareWord(JenisAkun(*P), stringToWord("Privat"))){
+        setWord(&JenisAkun(*P), "Publik");
+    }
+    else if (compareWord(JenisAkun(*P), stringToWord("Publik"))){
+        setWord(&JenisAkun(*P), "Privat");
+    }
+}
+
+/* *** PENAMPILAN NILAI *** */
+void displayProfil(Profile P, Word username){
+    /* Menampilkan profil pengguna dengan username "username" kecuali bagian foto profil */
+    printf("\n| Nama: ");
+    printWord(username);
+    printf("\n| Bio Akun: ");
+    printWord(Bio(P));
+    printf("\n| No HP: ");
+    printWord(NomorHP(P));
+    printf("\n| Weton: ");
+    printWord(Weton(P));
+}
+
+void lihatProfil(Profile P, Word username){
+    /* Menampilkan profil pengguna dengan username "username" */
+    printf("\n| Nama: ");
+    printWord(username);
+    printf("\n| Bio Akun: ");
+    printWord(Bio(P));
+    printf("\n| No HP: ");
+    printWord(NomorHP(P));
+    printf("\n| Weton: ");
+    printWord(Weton(P));
+    printf("\n\nFoto profil akun ");
+    printWord(username);
+    printf("\n");
+    displayFotoProfil(Foto(P));
 }
--- ADT/Profile/profile.h
+++ ADT/Profile/profile.h
@@ -1,25 +1,57 @@
+/* File: profile.h */
+/* ADT Profile yang terdiri dari bio akun, nomor HP, weton, jenis akun, dan foto profil dari pengguna */
+
 #ifndef PROFILE_H
 #define PROFILE_H
 
+/* *** ADT LAIN YANG DIPAKAI *** */
+#include "../Foto/foto.h"
 #include "../Boolean/boolean.h"
 #include "../MesinKata/wordmachine.h"
-#include "../Foto/foto.h"
 
-#define NAMA(p) (p).nama
-#define BIO(p) (p).bio
-#define NOHP(p) (p).noHp
-#define WETON(p) (p).weton
-#define FOTO(p) (p).foto
-
-typedef struct profile
-{
+/* *** DEFINISI TYPE PROFILE *** */
+typedef struct profile {
     Word bio;
-    Word noHp;
+    Word nomor_hp;
     Word weton;
+    Word jenis_akun;
     Foto foto;
 } Profile;
 
-void CreateProfile(Profile* profile);
-void DisplayProfile(Profile profile);
+/* ********* AKSES (Selektor) ********* */
+/* Jika P adalah Profile, maka akses elemen : */
+#define Bio(P) (P).bio
+#define NomorHP(P) (P).nomor_hp
+#define Weton(P) (P).weton
+#define JenisAkun(P) (P).jenis_akun
+#define Foto(P) (P).foto
+
+/* *** PROTOTYPE *** */
+/* *** PEMBUATAN DENGAN NILAI DEFAULT *** */
+void CreateProfil(Profile *P);
+/* Mambuat profil kosong dengan nilai-nilai default */
+/* *** PEMERIKSAAN NILAI *** */
+boolean isWetonValid(Word weton);
+/* Memeriksa apakah weton valid */
+boolean isNomorHPValid(Word nomor_hp);
+/* Memeriksa apakah nomor HP valid */
+boolean isAkunPrivat(Profile P);
+/* Memeriksa apakah jenis akun privat */
+boolean isAkunPublik(Profile P);
+/* Memeriksa apakah jenis akun publik */
+/* *** PENGUBAHAN NILAI *** */
+void changeBio(Profile *P, Word w);
+/* Mengubah bio berdasarkan masukan pengguna */
+void changeNomorHP(Profile *P, Word w);
+/* Mengubah nomor HP berdasarkan masukan pengguna */
+void changeWeton(Profile *P, Word w);
+/* Mengubah weton berdasarkan masukan pengguna */
+void changeJenisAkun(Profile *P);
+/* Mengubah jenis akun pengguna */
+/* *** PENAMPILAN NILAI *** */
+void displayProfil(Profile P, Word username);
+/* Menampilkan profil pengguna kecuali bagian foto profil */
+void lihatProfil(Profile P, Word username);
+/* Menampilkan profil pengguna dengan username "username" */
 
 #endif
