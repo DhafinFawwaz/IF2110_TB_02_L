9bbb77a0915ce9fc45d1d0fc54441bc282d61257
Nabila Shikoofa <nabilashikoofa@gmail.com>
2023-11-22 11:37:57 +0700

    feat: draf kicauan to kicauan

--- ADT/ListDinKicauan/listdinkicauan.h
+++ ADT/ListDinKicauan/listdinkicauan.h
@@ -46,7 +46,7 @@ typedef struct listdinkicauan
 // End of Struktur List Dinamis Kicauan
 
 // Fungsi dan Prosedur Kicauan
-void createKicauan(Kicauan * k, Word text, Word tagar);
+void createKicauan(Kicauan * k, Word text);
 
 void printKicauan(Kicauan kicauan);
 // End of Fungsi dan Prosedur Kicauan
