bd7f281f5531b9af5eddf8f5bd78044d4d37f610
Intermaze <rafikiprawhira@gmail.com>
2023-11-24 17:45:41 +0700

    chore: kelompokTeman descriptions

--- ADT/DSUKelompokTeman/dsuKelompokTeman.h
+++ ADT/DSUKelompokTeman/dsuKelompokTeman.h
@@ -9,7 +9,10 @@ typedef struct kelompokteman {
 	int parent[KELOMPOKTEMAN_CAPACITY];
 } kelompokTeman;
 
+//Mencari representatif id akun dengan index i
 int kelompokTeman_find(kelompokTeman kt, int i);
+
+//Menggabungkan kedua representatif, sehingga menggabungkannya dalam satu kelompok teman
 void kelompokTeman_union(kelompokTeman* kt,int i, int j);
 
 #endif
